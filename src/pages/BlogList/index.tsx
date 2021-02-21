86440a108f12c428b0b3124b4ed50289e34c4010
yalin.wang <yalin.wang@zmlearn.com>
2021-02-21 20:07:04 +0800

    commit the code about delete popup

--- src/pages/BlogList/index.tsx
+++ src/pages/BlogList/index.tsx
@@ -1,4 +1,4 @@
-import { Divider } from 'antd';
+import { Divider, Popconfirm } from 'antd';
 import { Link } from 'umi';
 import React, { useState, useRef } from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
@@ -10,11 +10,20 @@ interface BasicFormProps {
 }
 // react hooks
 const TableList: React.FC<BasicFormProps> = (props) => {
- 
+
   // const updateBlogById = (updateId: String) => {
   //   console.log('updateBlogById', updateId)
   // }
   const actionRef = useRef<ActionType>();
+  const haha = function (record: { id: any; }) {
+    console.log(record.id)
+    deleteblogs({ id: record.id }).then(res => {
+      console.log(res, 'res')
+      // location.reload()
+      // console.log(props.location.pathname.split('/list/')[1])
+              getblogs(location.pathname.split('/list/')[1])
+    })
+  }
   const columns: ProColumns<TableListItem>[] = [
     // {
     //   title: '板块',
@@ -60,26 +69,42 @@ const TableList: React.FC<BasicFormProps> = (props) => {
             }
           }}>编辑</Link>
           <Divider type="vertical" />
-          <a onClick={() => {
-            deleteblogs({ id: record._id });
-          }}>删除</a>
+          {/* <a onClick={() => {
+            console.log('delete')
+          }}>删除</a> */}
+          <Popconfirm
+            title="确定删除吗？"
+            // onConfirm={() => deleteblogs({ id: record._id }).then(res => {
+            //   console.log(props.location.pathname.split('/list/')[1])
+            //   getblogs(props.location.pathname.split('/list/')[1])
+            //   })}
+              onConfirm={() => haha({ id: record._id })}
+            // onCancel={canceldeleteblogs}
+            okText="Yes"
+            cancelText="No"
+          >
+            <a href="#">删除</a>
+          </Popconfirm>
         </>
       ),
     },
   ];
-
+  
   return (
     <PageHeaderWrapper>
       <ProTable<TableListItem>
         headerTitle="查询表格"
         actionRef={actionRef}
         rowKey="_id"
-        request={async () => {
-          // let blogs = await getblogs('js')
-          let blogs = await getblogs(props.location.pathname.split('/list/')[1])
-          // console.log('location', this.props.location.pathname.split('/list/')[1])
-          return blogs.data
-        }}
+        // request={async () => {
+        //   // let blogs = await getblogs('js')
+        //   let blogs = await getblogs(location.pathname.split('/list/')[1])
+        //   // console.log('location', this.props.location.pathname.split('/list/')[1])
+        //   return blogs.data
+        // }}
+        request={() => getblogs(location.pathname.split('/list/')[1]).then(res => {
+          return res.data
+        })}
         columns={columns}
         rowSelection={{}}
       />
